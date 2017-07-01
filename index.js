4452605bf8d0ec38db355e65aa17d5b104bd8ce8
Abhishek Deora <deoraabhishek1995@gmail.com>
2017-07-02 00:24:24 +0530

    setting up mongo

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var Db= require('mongodb').Db;
 var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 app.set('port', (process.env.PORT || 5000));
 app.set('mongo_url', (process.env.PORT || 'mongodb://root:password@ds145302.mlab.com:45302/youtube_queue_server'));
 
@@ -17,9 +18,10 @@ app.get('/', function(request, response) {
 	    assert.equal(null, err);
 	    db.collection('playlists').find(function(err, result) {
 		    assert.equal(null, err);
-		    res.send(result);
+		    //res.send(result);
 		    db.close();
 	    });
+	    response.send('hello world');
 	});
 		
 });
