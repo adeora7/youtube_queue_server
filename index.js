65d472ba4f6b85735b6a54a25fc00203ecaf84f0
Abhishek Deora <deoraabhishek1995@gmail.com>
2017-07-02 01:58:24 +0530

    API updated to add multiple routes and find condition modified

--- index.js
+++ index.js
@@ -13,17 +13,25 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/search/:query', function(request, response) {
+app.get(['/search/:query', '/search'], function(request, response) {
 	console.log(request.params.query);
 	MongoClient.connect('mongodb://root:password@ds145302.mlab.com:45302/youtube_queue_server', {native_parser:true}, function(err, db) {
 		assert.equal(null, err);
 		var collection = db.collection('playlists');
-		var all_playlists = []
-		var q1 = request.params.query;
-		var q2 = '^'+request.params.query;
-	    	var stream = collection.find({ $or: [ {"name": {'$regex': q1 }}, {"PID": {'$regex': q2 }}   ] }).stream();
-	    	stream.on("data", function(item){ 
-		    	console.log("data came ");// + JSON.stringify(item));
+		var all_playlists = [];
+		if( typeof request.query.params != 'undefined'){
+			var q1 = request.params.query;
+			var q2 = '^'+request.params.query;
+		}
+		else
+		{
+			var q1="";
+			var q2="";
+		}
+	    	var stream = collection.find({ $or: [ {"name": {'$regex': q1 }}, {"PID": {'$regex': q2 }}   ] }).limit(30).stream();
+		
+		stream.on("data", function(item){ 
+		    	console.log("data came ");
 			all_playlists.push(item);
 		    
 		});
