e8764fc8982dc4933c9b042c2c74c973857132c4
Abhishek Deora <deoraabhishek1995@gmail.com>
2017-07-02 00:31:15 +0530

    mongo url correction

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var Db= require('mongodb').Db;
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 app.set('port', (process.env.PORT || 5000));
-app.set('mongo_url', (process.env.PORT || 'mongodb://root:password@ds145302.mlab.com:45302/youtube_queue_server'));
+app.set('mongo_url', (process.env.MONGO_URI || 'mongodb://root:password@ds145302.mlab.com:45302/youtube_queue_server'));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
 	
-	MongoClient.connect(mongo_url, {native_parser:true}, function(err, db) {
+	MongoClient.connect('mongodb://root:password@ds145302.mlab.com:45302/youtube_queue_server', {native_parser:true}, function(err, db) {
 	    assert.equal(null, err);
 	    db.collection('playlists').find(function(err, result) {
 		    assert.equal(null, err);
